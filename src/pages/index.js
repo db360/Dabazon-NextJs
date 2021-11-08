ae80c87d66661882dfbe1ca06d3579dfd3b3d70a
db360 <da.b@hotmail.es>
2021-11-08 19:42:34 +0100

    Products

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,21 +7,21 @@ import ProductFeed from "../components/ProductFeed";
 
 export default function Home({products}) {  // pasando props desestructurados desde fetch
   return (
-    <div>
-      <Head className="bg-gray-100">
+    <div className="bg-gray-100">
+      <Head >
         <title>DaB Music BlockChain</title>
       </Head>
 
       {/* Header */}
-      <Header />
 
+      <Header />
       <main className="max-w-screen-2xl mx-auto">
         {/* Banner */}
         <Banner />
 
         {/* Product Feed */}
-        <ProductFeed products={products} /> {/* Para Mandarlo hacia ProductFeed component como prop */}
 
+        <ProductFeed products={products} /> {/* Para Mandarlo hacia ProductFeed component como prop */}
       </main>
     </div>
   );
